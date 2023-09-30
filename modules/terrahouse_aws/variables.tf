ec51dab9341fad423a76c1274d4dfbcfe3612983
David Richey <davidsacc@gmail.com>
2023-09-30 00:39:22 -0400

    #27 enabled static website hosting, uploaded index and error file via terraform (#28)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -1,6 +1,7 @@
 variable "user_uuid" {
   description = "User UUID"
   type = string
+
   validation {
     condition = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
     error_message = "Invalid user UUID format. It should be in the form of 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' where x is a hexadecimal digit."
@@ -9,11 +10,30 @@ variable "user_uuid" {
 
 variable "bucket_name" {
   description = "Name of the AWS S3 bucket"
-  
   type = string
 
   validation {
     condition = length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63
     error_message = "Bucket name length must be between 3 and 63 characters."
+  }
+}
+
+variable "index_html_filepath" {
+  description = "The file path for index.html"
+  type = string
+
+  validation {
+    condition = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exists."
+  }
+}
+
+variable "error_html_filepath" {
+  description = "The file path for error.html"
+  type = string
+
+  validation {
+    condition = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exists."
   }
 }
