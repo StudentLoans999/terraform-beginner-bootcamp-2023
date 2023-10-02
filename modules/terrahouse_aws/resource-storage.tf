62e09477853bdccec51be21641a94c09b93464fc
David Richey <davidsacc@gmail.com>
2023-10-02 17:55:06 +0000

    yes!

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   }
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
+/*# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
@@ -41,7 +41,7 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
   
   etag = filemd5("${path.root}${var.error_html_filepath}")
-}
+}*/
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
     bucket = aws_s3_bucket.website_bucket.bucket
