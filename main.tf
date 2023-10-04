7fe4de04be17e7f38d5ead21a399c48391712e26
David Richey <davidsacc@gmail.com>
2023-10-04 12:47:58 -0400

    44 terratowns provider (#48)
    * #44 created main.go
    
    * #44 wip setting up the skeleton for creating a custom terraform provider
    
    * #44 implement terraform proivder block in terraform
    
    * #44 setup skeleton for resource
    
    * Update .gitpod.yml
    
    * #44 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -29,3 +29,14 @@ provider "terratowns" {
   content_version = var.content_version
   assets_path = var.assets_path
 }*/
+
+resource "terratowns_home" "home" {
+  name = "My favorite video games"
+  description = <<DESCRIPTION
+Warcraft 3 and Gears of War 3 
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "4sdfz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
